9832e2eae7e6f495262531f11587f6edc2c1f1d9
Mastermehan <mastermehan1984@gmail.com>
2021-09-13 21:50:28 +0300

    работа с числами с трим

---
+++ Number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Number {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(x -> x > 0)  //Отфильтруйте положительные числа
+                .filter(x -> x % 2 == 0)  //Найдите среди этих положительных чисел четные.
+                .sorted(Comparator.naturalOrder()) //Отсортируйте отфильтрованные числа в порядке возрастания
+                .forEach(System.out::println);
+
+    }
+}
